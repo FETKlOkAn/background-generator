df7de0e45a5650164e575876203302a544ce7c92
tomas maxim <svkmongolec@gmail.com>
2024-02-24 15:10:14 +0100

    Bg gen with .jason file, npm and node !

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var _ = require('lodash');
+console.log(_);
+
+var array = [3,5,7,4,3,1,8];
+console.log('answer',_.without(array, 5));
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
